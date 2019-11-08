e114ee05389b88d12b97c247714cd6ea78b6130d
Dan Olson <dano7900@gmail.com>
2019-11-08 16:30:43 -0500

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,9 @@ event.waitUntil(
     caches.open(cacheName).then(function(cache) {
         return cache.addAll(
             [
-                '/index.html',
-                '/stylesheet.css',
-                '/ShaderLoader.js',
+                'index.html',
+                'stylesheet.css',
+                'ShaderLoader.js',
             ]
          );
     })
