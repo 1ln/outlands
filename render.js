fc56670581e9f65dac101abbefdd656d1942746a
Dan Olson <dano7900@gmail.com>
2019-11-13 16:25:13 -0500

    refactor render.js

--- render.js
+++ render.js
@@ -1,139 +1,107 @@
+let display_fullscreen = false;
+let orbit_controls = false;
+
 let canvas = $('#canvas')[0];
 
-let camera,scene,renderer;
-let uniforms,geometry,material,mesh;
-let controls;
+let w,h;
+if(display_fullscreen === true) {
+    w = window.innerWidth;
+    h = window.innerHeight;
+} else {
+    w = 512;
+    h = 512;
+}
 
-let mouse_pressed = false;
-
-let mouse = new THREE.Vector2();
-let raycaster = new THREE.Raycaster();
-
-let clock          = new THREE.Clock();
-
-let camera_target  = new THREE.Vector3(0.0);
-
-let light  = new THREE.Vector3(0.0,0.0,0.0);
-
-let mouse_ray_near = new THREE.Vector3(0.0);
-let mouse_ray_far  = new THREE.Vector3(0.0);
-
-let orbit_target   = new THREE.Quaternion();
-
-let sphere         = new THREE.Vector3(0.0);
-let spherical      = new THREE.Spherical();
-
-let distance = 0.1;
-let new_scene = false;
-
-let w = window.innerWidth;
-let h = window.innerHeight;
 canvas.width  = w;
-canvas.height = h; 
+canvas.height = h;
 
 let aspect = w/h;
 
-let prng = new Math.seedrandom();
+let renderer = new THREE.WebGLRenderer({canvas:canvas});
 
-let hash = prng();
-console.log(hash);
+let nhash = new Math.seedrandom();
 
-spherical.theta  = prng() * Math.PI * 2.0;
-spherical.phi    = Math.acos( (2.0 * prng() ) - 1.0);
-spherical.radius = 1.0;
+let mouse_pressed = false;
+let mouse = new THREE.Vector2();
 
-let render_scene = {
+let raycaster = new THREE.Raycaster();
+let clock = new THREE.Clock();
+let cam_target  = new THREE.Vector3(0.0);
+let light = new THREE.Vector3(0.0,10.0,0.0);
+let mouse_ray_near = new THREE.Vector3(0.0);
+let mouse_ray_far  = new THREE.Vector3(0.0);
+let orbit_target   = new THREE.Quaternion();
 
-    hash           : hash,
-    octaves        : Math.floor(prng()*4.0) + 1,
-    epsilon        : 0.001,
-    trace_distance : 1000,
-    march_steps    : 32      
+let spherical        = new THREE.Spherical();
+    spherical.theta  = nhash() * Math.PI * 2.0;
+    spherical.phi    = Math.acos( (2.0 * nhash() ) - 1.0);
+    spherical.radius = 1.0;
 
-};
+let epsilon = 0.001;
+let trace_distance = 1000.0;
 
-let lighting = {
-    ambient_color   : [0.0,0.0,0.0],
-    specular_color  : [prng(),prng(),prng()],
-    shininess       : prng() * 100.0,
-    diffuse_color   : [prng(),prng(),prng()],
-    diffuse_b       : [prng(),prng(),prng()],
-    diffuse_c       : [prng(),prng(),prng()],
-    diffuse_d       : [prng(),prng(),prng()],
-    diffuse_distort : Math.round(prng()) ? true : false,
-    diffuse_fractal : Math.round(prng()) ? true : false,
-    diffuse_cell    : Math.round(prng()) ? true : false
+let shininess = 100.0;
+ 
+let ambient_color   = new THREE.Vector3(0.0);
 
-};
+let specular_color  = new THREE.Vector3(nhash(),nhash(),nhash());
+//let shininess      = rh() * 100.0;
 
-let displace = {
-    sin3            : Math.round(prng()) ? 1.0 : 0.0,
-    fractal         : Math.round(prng()) ? 1.0 : 0.0,
-    cell            : false,
-    cell_iterations : 6.0,
-    noise_rounding  : 0.25
-};
+let diffuse_color   = new THREE.Vector3( nhash(),nhash(),nhash());
+let diffuse_b       = new THREE.Vector3( nhash(),nhash(),nhash());
+let diffuse_c       = new THREE.Vector3( nhash(),nhash(),nhash());
+let diffuse_d       = new THREE.Vector3( nhash(),nhash(),nhash());
+   
+let diffuse_distort = Math.round(nhash()) ? true : false;
+let diffuse_fractal = Math.round(nhash()) ? true : false;
+let diffuse_cell    = Math.round(nhash()) ? true : false;
 
-init();
-//animate();
-//guiUpdated();
+let displace_sin3   = Math.round(nhash()) ? 1.0 : 0.0;
+let fractal         = Math.round(nhash()) ? 1.0 : 0.0;
+let cell            = false;
 
-function init() {
+let camera = new THREE.PerspectiveCamera(45.0, canvas.width/canvas.height,1,2500);
+    camera.position.set(3.0,2.5,-1.0);
+    camera.lookAt(0.0);
 
-//canvas.width = w;
-//canvas.height = h;
+let controls = new THREE.OrbitControls(camera,canvas);
+    controls.minDistance = 1.5;
+    controls.maxDistance = 24.5;
+    controls.target = cam_target;
+    controls.enableDamping = true;
+    controls.enablePan = false;
+    //controls.maxPolarAngle = .95;
+    controls.enabled = false; 
 
-hash = prng();
+let scene = new THREE.Scene();
+let geometry = new THREE.PlaneBufferGeometry(2,2);
+let mesh,material;
 
-clock.start();
-
-renderer = new THREE.WebGLRenderer({ canvas:canvas });
-
-camera = new THREE.PerspectiveCamera(45.0, canvas.width/canvas.height,1,2500);
-
-camera.position.set(3.0,2.5,-1.0);
-camera.lookAt(0.0);
-
-controls = new THREE.OrbitControls(camera,canvas);
-controls.minDistance = 1.5;
-controls.maxDistance = 24.5;
-controls.target = camera_target;
-controls.enableDamping = true;
-controls.enablePan = false;
-//controls.maxPolarAngle = .95;
-controls.enabled = false; 
-
-light  = new THREE.Vector3(0.0,1.0,0.0);
-
-scene = new THREE.Scene();
-
-geometry = new THREE.PlaneBufferGeometry(2,2);
-
-uniforms = {
+let uniforms = {
 
     "u_time"             : { value : 1.0 },
     "u_resolution"       : new THREE.Uniform(new THREE.Vector2(w,h)),
     "u_mouse"            : new THREE.Uniform(new THREE.Vector2()),
     "u_mouse_pressed"    : { value : mouse_pressed },
-    "u_camera_target"    : new THREE.Uniform(new THREE.Vector3(camera_target)),
+    "u_camera_target"    : new THREE.Uniform(new THREE.Vector3(cam_target)),
     "u_light"            : new THREE.Uniform(new THREE.Vector3(light)),
     "u_mouse_ray_far"    : new THREE.Uniform(new THREE.Vector3(0.0,0.0,0.0)),
     "u_mouse_ray_near"   : new THREE.Uniform(new THREE.Vector3(0.0)),
-    "u_hash"             : { value: hash },
-    "u_octaves"          : { value: render_scene.octaves },
-    "u_epsilon"          : { value: render_scene.epsilon },
-    "u_trace_distance"   : { value: render_scene.trace_distance },
-    "u_march_steps"      : { value: render_scene.march_steps },
-    "u_specular_color"   : new THREE.Uniform(new THREE.Vector3(lighting.specular_color)),
-    "u_diffuse_color"    : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_color)),
-    "u_ambient_color"    : new THREE.Uniform(new THREE.Vector3(lighting.ambient_color)),
-    "u_shininess"        : { value: lighting.shininess },
-    "u_diffuse_b"        : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_b)),
-    "u_diffuse_c"        : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_c)),
-    "u_diffuse_d"        : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_d)),
-    "u_diffuse_distort"  : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_distort)),
-    "u_diffuse_fractal"  : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_fractal)),
-    "u_diffuse_cell"     : new THREE.Uniform(new THREE.Vector3(lighting.diffuse_cell)),
+    "u_hash"             : { value: nhash },
+    "u_octaves"          : { value: 0.0 },
+    "u_epsilon"          : { value: epsilon },
+    "u_trace_distance"   : { value: trace_distance },
+    //"u_march_steps"      : { value: march_steps },
+    "u_specular_color"   : new THREE.Uniform(new THREE.Vector3(specular_color)),
+    "u_diffuse_color"    : new THREE.Uniform(new THREE.Vector3(diffuse_color)),
+    "u_ambient_color"    : new THREE.Uniform(new THREE.Vector3(ambient_color)),
+    "u_shininess"        : { value: shininess },
+    "u_diffuse_b"        : new THREE.Uniform(new THREE.Vector3(diffuse_b)),
+    "u_diffuse_c"        : new THREE.Uniform(new THREE.Vector3(diffuse_c)),
+    "u_diffuse_d"        : new THREE.Uniform(new THREE.Vector3(diffuse_d)),
+    "u_diffuse_distort"  : new THREE.Uniform(new THREE.Vector3(diffuse_distort)),
+    "u_diffuse_fractal"  : new THREE.Uniform(new THREE.Vector3(diffuse_fractal)),
+    "u_diffuse_cell"     : new THREE.Uniform(new THREE.Vector3(diffuse_cell)),
     "u_texture"          : { type : "t", value: texture },
     "u_swipe_left"       : { value: 0.0 },
     "u_swipe_right"      : { value: 0.0 },
@@ -142,7 +110,6 @@ uniforms = {
 
 
 };   
-}
 
 ShaderLoader("render.vert","render.frag",
     function(vertex,fragment) {
@@ -219,33 +186,33 @@ ShaderLoader("render.vert","render.frag",
         //camera.translateY(distance);
 
 
-        uniforms["u_time"].value            = performance.now();
-        uniforms["u_mouse"].value           = mouse;
-        uniforms["u_mouse_pressed"].value   = mouse_pressed;
-        uniforms["u_camera_target"].value   = camera_target;
-        uniforms["u_light"].value           = light;
-        uniforms["u_mouse_ray_far"].value   = mouse_ray_far;
-        uniforms["u_mouse_ray_near"].value  = mouse_ray_near;
-        uniforms["u_hash"].value            = render_scene.hash;
-        uniforms["u_octaves"].value         = render_scene.octaves;
-        uniforms["u_epsilon"].value         = render_scene.epsilon;
-        uniforms["u_trace_distance"].value  = render_scene.trace_distance;
-        uniforms["u_march_steps"].value     = render_scene.march_steps;
-        uniforms["u_ambient_color"].value   = lighting.ambient_color;
-        uniforms["u_diffuse_color"].value   = lighting.diffuse_color;
-        uniforms["u_specular_color"].value  = lighting.specular_color;
-        uniforms["u_shininess"].value = lighting.shininess;
-        uniforms["u_diffuse_b"].value = lighting.diffuse_b;
-        uniforms["u_diffuse_c"].value = lighting.diffuse_c;
-        uniforms["u_diffuse_d"].value = lighting.diffuse_d;
-        uniforms["u_diffuse_distort"].value = lighting.diffuse_distort;
-        uniforms["u_diffuse_fractal"].value = lighting.diffuse_fractal;
-        uniforms["u_diffuse_cell"].value = lighting.diffuse_cell;
-        uniforms["u_texture"].value = texture;
-        uniforms["u_swipe_left"].value = swipeLeft();
-        uniforms["u_swipe_right"].value = swipeRight();
-        uniforms["u_swipe_up"].value = swipeUp();
-        uniforms["u_swipe_down"].value = swipeDown();
+        uniforms["u_time"            ].value = performance.now();
+        uniforms["u_mouse"           ].value = mouse;
+        uniforms["u_mouse_pressed"   ].value = mouse_pressed;
+        uniforms["u_camera_target"   ].value = cam_target;
+        uniforms["u_light"           ].value = light;
+        uniforms["u_mouse_ray_far"   ].value = mouse_ray_far;
+        uniforms["u_mouse_ray_near"  ].value = mouse_ray_near;
+        uniforms["u_hash"            ].value = nhash;
+        uniforms["u_octaves"         ].value = 0.0;
+        uniforms["u_epsilon"         ].value = epsilon;
+        uniforms["u_trace_distance"  ].value = trace_distance;
+     //   uniforms["u_march_steps"     ].value = march_steps;
+        uniforms["u_ambient_color"   ].value = ambient_color;
+        uniforms["u_diffuse_color"   ].value = diffuse_color;
+        uniforms["u_specular_color"  ].value = specular_color;
+        uniforms["u_shininess"       ].value = shininess;
+        uniforms["u_diffuse_b"       ].value = diffuse_b;
+        uniforms["u_diffuse_c"       ].value = diffuse_c;
+        uniforms["u_diffuse_d"       ].value = diffuse_d;
+        uniforms["u_diffuse_distort" ].value = diffuse_distort;
+        uniforms["u_diffuse_fractal" ].value = diffuse_fractal;
+        uniforms["u_diffuse_cell"    ].value = diffuse_cell;
+        uniforms["u_texture"         ].value = texture;
+        uniforms["u_swipe_left"      ].value = swipeLeft();
+        uniforms["u_swipe_right"     ].value = swipeRight();
+        uniforms["u_swipe_up"        ].value = swipeUp();
+        uniforms["u_swipe_down"      ].value = swipeDown();
 
         //if(camera.position.distanceTo(new THREE.Vector3(0.0,0.0,0.0)) > 1.5) {
         //console.log("test"); 
