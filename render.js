d8705d3f82fa97c88b5477b80cc3982cb429876c
Dan Olson <dano7900@gmail.com>
2019-12-23 23:36:02 -0500

    add 3d texture

--- render.js
+++ render.js
@@ -1,27 +1,27 @@
 let w,h;
 
 let canvas,context;
+
 let renderer;
+let render;
+let uniforms;
 
 let nhash,hash;  
-let mouse_pressed;
+let tex_size;
+let noise;
+let noise_texture;
 
-let mouse = new THREE.Vector2(0.0);
-
+let mouse_pressed,mouse;
 let swipe_dir;
 
 let controls;
 
 let cam,scene,geometry,mesh,mat;
-let aspect;
 
 let cam_target;
 
 let delta;
 let clock;
-
-let uniforms;
-let render;
 
 function init() {
 
@@ -36,12 +36,7 @@ canvas.height = h;
 
 renderer = new THREE.WebGLRenderer({canvas:canvas,context:context});
 
-mouse_pressed = 0;
-
-aspect = w/h;
-trace_distance = 1000.0;
-
-cam = new THREE.PerspectiveCamera(45.0,aspect,0.0,trace_distance);
+cam = new THREE.PerspectiveCamera(1.0,w/h,0.0,1000.0);
 
 clock = new THREE.Clock(); 
 delta = 0.0;
@@ -49,6 +44,22 @@ delta = 0.0;
 nhash = new Math.seedrandom();
 hash = nhash();
 
+tex_size = 32*32*32;
+noise = new Float32Array(tex_size);
+
+for(let i = 0; i < size; i++) {
+
+    noise[i]     = nhash();
+    noise[i + 1] = nhash();
+    noise[i + 2] = nhash();
+
+}
+
+noise_texture = new THREE.DataTexture3D(noise,32,32,32);
+console.log(noise_texture);
+
+mouse = new THREE.Vector2(0.0); 
+mouse_pressed = 0;
 swipe_dir = 0;
 
 cam.position.set(0.0,0.0,5.0); 
@@ -73,7 +84,8 @@ uniforms = {
     "u_mouse_pressed"       : { value : mouse_pressed },
     "u_swipe_dir"           : { value : swipe_dir }, 
     "u_cam_target"          : new THREE.Uniform(new THREE.Vector3(cam_target)),
-    "u_hash"                : { value: hash }
+    "u_hash"                : { value: hash },
+    "u_noise_tex"           : { value: noise_texture }
 
 };   
 
@@ -109,13 +121,16 @@ ShaderLoader("render.vert","render.frag",
         if(swipeUp()    === true) { swipe_dir = 2; }
         if(swipeRight() === true) { swipe_dir = 3; }
         if(swipeDown()  === true) { swipe_dir = 4; }
-  */    
+  */   
+
+ 
         uniforms["u_time"                ].value = performance.now();
         uniforms["u_mouse"               ].value = mouse;
         uniforms["u_mouse_pressed"       ].value = mouse_pressed;
         uniforms["u_swipe_dir"           ].value = swipe_dir;
         uniforms["u_cam_target"          ].value = cam_target;
         uniforms["u_hash"                ].value = hash;
+        uniforms["u_noise_tex"           ].value = noise_texture;       
 
         //controls.update();
         renderer.render(scene,cam);
